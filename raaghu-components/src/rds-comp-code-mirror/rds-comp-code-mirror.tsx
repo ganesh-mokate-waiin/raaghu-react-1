d289c18b3167756573ea22d4b9d39aa3fa9ba22b
Alok Nakhate <alok.nakhate@waiin.com>
2023-06-01 11:11:45 +0530

    Build issues are resolved

--- raaghu-components/src/rds-comp-code-mirror/rds-comp-code-mirror.tsx
+++ raaghu-components/src/rds-comp-code-mirror/rds-comp-code-mirror.tsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/monokai.css';
 import 'codemirror/mode/javascript/javascript';
@@ -6,33 +6,35 @@ import 'codemirror/mode/javascript/javascript';
 // import { Controlled as CodeMirror } from 'react-codemirror2'; // Import the React wrapper for CodeMirror
 
 export interface codemirrorProps {
-    onBeforeChange?: (editor:any, data:any, value:any) =>any
-    code:string
+  onBeforeChange?: (editor: any, data: any, value: any) => any
+  code: string
 }
- const MyCodeMirrorComponent = (props:codemirrorProps) => {
+const MyCodeMirrorComponent = (props: codemirrorProps) => {
 
-    const [code, setCode] = useState(props.code);
-    const options = {
-        theme: 'default',
-      mode: 'javascript',
-      lineNumbers: true,
-      tabSize: 2,
-     };
-     const handleBeforeChange = (editor:any, data:any, value:any) => {
-        props.onBeforeChange&& props.onBeforeChange(editor,data,value)
-        setCode(value);
-      };
-    
-      useEffect(() => {
-        setCode(props.code)
-      }, [props.code])
-    return 
-    // (
-      // <CodeMirror
-      //   value={code}
-      //   options={options}
-      //   onBeforeChange={handleBeforeChange}
-      // />
-    // );
+  const [code, setCode] = useState(props.code);
+  const options = {
+    theme: 'default',
+    mode: 'javascript',
+    lineNumbers: true,
+    tabSize: 2,
   };
-  export default MyCodeMirrorComponent
+  const handleBeforeChange = (editor: any, data: any, value: any) => {
+    props.onBeforeChange && props.onBeforeChange(editor, data, value)
+    setCode(value);
+  };
+
+  useEffect(() => {
+    setCode(props.code)
+  }, [props.code]);
+
+  return (
+    <>
+      {/* <CodeMirror
+        value={code}
+        options={options}
+        onBeforeChange={handleBeforeChange}
+      /> */}
+    </>
+  );
+};
+export default MyCodeMirrorComponent
